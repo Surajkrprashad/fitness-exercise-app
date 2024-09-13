af03a2d155c7e21f15ddd013927c02093ea8861e
Suraj Kumar <surajkrprashad@gmail.com>
2024-09-13 15:57:17 +0530

    Fix ESLint warnings and update dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
